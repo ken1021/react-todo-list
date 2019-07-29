9333166c83783ca0b1eab627ae09bbba924afcc6
KEN <cengchunxiao@KendeMacBook.local>
2019-07-29 20:36:03 +0800

    Updates

---
+++ precache-manifest.9e998679b2b6e4ce386e119c53bd58e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05e70a55ddd28c8ee6d0c79827f8634",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "f8c7a1441e02c7d9c249",
+    "url": "/react-todo-list/static/css/main.232d1e3b.chunk.css"
+  },
+  {
+    "revision": "74cb4ea5d6daa89bb286",
+    "url": "/react-todo-list/static/js/2.4da02fa2.chunk.js"
+  },
+  {
+    "revision": "f8c7a1441e02c7d9c249",
+    "url": "/react-todo-list/static/js/main.15c10d97.chunk.js"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  }
+]);
